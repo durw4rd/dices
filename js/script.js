1853c211e6ee33e44819d579053e07f7bd1ad646
durw4rd <misa.fasa@gmail.com>
2016-07-27 10:07:13 +0200

    change file structure as the project outgrown the original purpose; add background and legit header + footer; add transition effect on each throw

---
+++ js/script.js
@@ -0,0 +1,66 @@
+var main = function() {
+
+  var hideFaces = function() {
+    $('.one').hide();
+    $('.two').hide();
+    $('.three').hide();
+    $('.four').hide();
+    $('.five').hide();
+    $('.six').hide();
+  };
+
+  hideFaces();
+
+  $('.throw-button').click(function() {
+    TweenMax.to($('.thrown'), 0.2, {x: '150px', opacity: 0});
+    TweenMax.to($('.thrown'), 0.1, {delay: 0.2, x: '0px'});
+
+    setTimeout(function() {
+      $('*').removeClass('thrown');
+      $('*').removeClass('selected-dice');
+      hideFaces();
+
+      var i = Math.random();
+      console.log(i);
+      if(i <= 0.16) {
+        $('.one').addClass('thrown');
+        // $('.one').show();
+        $('.one-thumb').addClass('selected-dice');
+      } else if(i > 0.16 && i <= 0.33) {
+        $('.two').addClass('thrown');
+        // $('.two').show();
+        $('.two-thumb').addClass('selected-dice');
+      } else if(i > 0.33 && i <= 0.5) {
+        $('.three').addClass('thrown');
+        // $('.three').show();
+        $('.three-thumb').addClass('selected-dice');
+      } else if(i > 0.5 && i <= 0.66) {
+        $('.four').addClass('thrown');
+        // $('.four').show();
+        $('.four-thumb').addClass('selected-dice');
+      } else if(i > 0.66 && i <= 0.83) {
+        $('.five').addClass('thrown');
+        // $('.five').show();
+        $('.five-thumb').addClass('selected-dice');
+      } else {
+        $('.six').addClass('thrown');
+        // $('.six').show();
+        $('.six-thumb').addClass('selected-dice');
+      };
+      $('.thrown').show();
+      TweenMax.from($('.thrown'), 0.3, {x: '-150px'});
+      TweenMax.to($('.thrown'), 0.3, {opacity: 1});
+    }, 300);
+
+    // TweenMax.from($('.thrown'), 0.3, {delay: 0.3, x: '-150px'});
+
+  });
+
+  if ($(window).width() < 1024) {
+     $('.thumb').hide();
+  } else {
+    $('.thumb').show();
+  };
+}
+
+$(document).ready(main);
